cd791ad4e7e7b0915f6071cbfc45c0e70c44a4ef
Nate Pink <nate.pink@craftjack.com>
2017-12-25 23:55:03 -0500

    day 14 complete

--- Day14x2.cs
+++ Day14x2.cs
@@ -5,7 +5,7 @@ using Xunit;
 
 namespace AdventOfCode
 {
-    public class Day14x1
+    public class Day14x2
     {
         /* 
         --- Day 14: Disk Defragmentation ---
@@ -36,14 +36,41 @@ In this example, 8108 squares are used across the entire 128x128 grid.
 
 Given your actual key string, how many squares are used?
 
-Your puzzle input is jzgqcdpd.*/
+Your puzzle input is jzgqcdpd.
+
+Your puzzle answer was 8074.
+
+The first half of this puzzle is complete! It provides one gold star: *
+
+--- Part Two ---
+
+Now, all the defragmenter needs to know is the number of regions. A region is a group of used squares that are all adjacent, not including diagonals. Every used square is in exactly one region: lone used squares form their own isolated regions, while several adjacent squares all count as a single region.
+
+In the example above, the following nine regions are visible, each marked with a distinct digit:
+
+11.2.3..-->
+.1.2.3.4   
+....5.6.   
+7.8.55.9   
+.88.5...   
+88..5..8   
+.8...8..   
+88.8.88.-->
+|      |   
+V      V   
+Of particular interest is the region marked 8; while it does not appear contiguous in this small view, all of the squares marked 8 are connected when considering the whole 128x128 grid. In total, in this example, 1242 regions are present.
+
+How many regions are present given your key string?
+
+Your puzzle input is still jzgqcdpd.
+*/
         [Fact]
         public void First()
         {
-            var expected = 8108;
+            var expected = 1242;
             var input = "flqrgnkx";
 
-            var actual = FindCount(input);
+            var actual = FindRegions(input);
 
             Assert.Equal(expected, actual);
         }
@@ -51,15 +78,76 @@ Your puzzle input is jzgqcdpd.*/
         [Fact]
         public void Actual()
         {
-            var expected = 8074;
+            var expected = 1212;
             var input = "jzgqcdpd";
 
-            var actual = FindCount(input);
+            var actual = FindRegions(input);
 
             Assert.Equal(expected, actual);
         }
 
-        private int FindCount(string input)
+        private int FindRegions(string input)
+        {
+            var grid = FindBinary(input);
+            
+            var points = new List<Point>();
+            for (var y = 0; y < grid.Count; y++)
+            {
+                var line = grid[y];
+                for (var x = 0; x < line.Length; x++)
+                {
+                    if (line[x] == '1')
+                    {
+                        points.Add(new Point(x,y));
+                    }
+                }
+            }
+
+            var visited = new List<Point>();
+            var regions = new Dictionary<Point, List<Point>>();
+            
+            Point nextPoint = null;
+            nextPoint = points.First();
+            while(nextPoint != null)
+            {
+                var current = nextPoint.Clone();
+                regions.Add(current, new List<Point>());
+                Search(current, current, points, regions, visited);
+
+                nextPoint = points.FirstOrDefault(x => !visited.Contains(x));
+            }
+
+            return regions.Count();
+        }
+
+        private void Search(Point current, Point currentRegion, List<Point> points, Dictionary<Point, List<Point>> regions, List<Point> visited)
+        {
+            regions[currentRegion].Add(current);
+            visited.Add(current);
+
+            var point = new Point(current.X + 1, current.Y);
+            if (points.Contains(point) && !visited.Contains(point))
+            {
+                Search(point, currentRegion, points, regions, visited);
+            }
+            point = new Point(current.X - 1, current.Y);
+            if (points.Contains(point) && !visited.Contains(point))
+            {
+                Search(point, currentRegion, points, regions, visited);
+            }
+            point = new Point(current.X, current.Y + 1);
+            if (points.Contains(point) && !visited.Contains(point))
+            {
+                Search(point, currentRegion, points, regions, visited);
+            }
+            point = new Point(current.X, current.Y - 1);
+            if (points.Contains(point) && !visited.Contains(point))
+            {
+                Search(point, currentRegion, points, regions, visited);
+            }
+        }
+
+        private List<string> FindBinary(string input)
         {
             var hashes = new List<string>();
             for (var i = 0; i < 128; i++)
@@ -70,9 +158,7 @@ Your puzzle input is jzgqcdpd.*/
 
             var result = hashes
                             .Select(ToBinaryFromHex)
-                            .Aggregate((x, collector )=> collector += x)
-                            .Select(x => x == '1' ? 1 : 0)
-                            .Sum();
+                            .ToList();
 
             return result;
         }
